f6589a529dcc27317c0fb7cd454266fd0c7e07d8
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-10-29 12:14:49 +0300

    Initial Commit

---
+++ ColorMaker_SwiftUI_App/ColorMaker_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMaker_SwiftUI_AppApp.swift
+//  ColorMaker_SwiftUI_App
+//
+//  Created by Константин Кириллов on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorMaker_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
